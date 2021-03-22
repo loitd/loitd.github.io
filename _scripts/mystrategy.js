1f001cc009a1c167ab7ddce9fcb642a73e32fb96
loitd <loitranduc@gmail.com>
2021-03-22 11:17:40 +0700

    updated-by-loitd

--- _scripts/mystrategy.js
+++ _scripts/mystrategy.js
@@ -11,13 +11,15 @@
 // - MACD cho tín hiệu cắt giữa Singal & MACD khớp với trend đã confirm ở trên (trend buy -> buy signal)
 // - Stop bằng đỉnh đáy gần nhất
 // - Target bằng Stop * 2
-// - Chấp nhận slippage=2
+// - Chấp nhận slippage=2, commission = 4
 // - Một tuần chỉ trade tối đa 1 lệnh -> 7D -> 168H -> 10080M
 
 //@version=4
 // STEP 1. Define strategy settings. default_qty_value tối thiểu = 1, nếu nhỏ hơn sẽ không có trade nào.
 // calc_on_every_tick = TRUE: để đảm bảo tính hết các trường hợp có 1 thời điểm nhất định giá tụt/tăng rất sâu/cao sau đó lại hồi về. Nếu chỉ tính giá đóng cửa sẽ không sát thực tế.
-strategy(title="Leo_Strategy", overlay=true, initial_capital=1000, default_qty_type=strategy.cash, default_qty_value=5, pyramiding=0, slippage=2, calc_on_every_tick=true)
+// cho cả giá commission khá cao, để yêu cầu strategy phải thật sự tốt mới có khả năng đáp ứng.
+// default_qty_value = 1/100 tổng vốn.
+strategy(title="Leo_Strategy", overlay=true, initial_capital=1000, default_qty_type=strategy.cash, default_qty_value=10, pyramiding=0, slippage=2, calc_on_every_tick=true, commission_type=strategy.commission.cash_per_order, commission_value=4)
 // Actually the highest supported minute resolution is “1440” (which is the number of minutes in 24 hours).
 // Requesting data of "1h" or "1H" resolution would result in an error. Use "60" instead.
 tf_anchor1 = input(title="Anchor 1 timeframe", type=input.resolution, defval="240")
@@ -26,6 +28,11 @@ ma1_len = input(title="MA1 Len", type=input.integer, defval=13, minval=1, maxval
 ma2_len = input(title="MA2 Len", type=input.integer, defval=21, minval=1, maxval=9999)
 ma3_len = input(title="MA3 Len", type=input.integer, defval=34, minval=1, maxval=9999)
 ma4_len = input(title="MA4 Len", type=input.integer, defval=89, minval=1, maxval=9999)
+// Squeeze declare
+length = input(20, title="BB Length")
+mult = input(2.0,title="BB MultFactor")
+lengthKC=input(20, title="KC Length")
+multKC = input(1.5, title="KC MultFactor")
 
 // tradeDirection = input(title="Trade Direction", options=["Long", "Short", "Both"], defval="Both")
 // Options that configure the backtest date range
@@ -33,9 +40,10 @@ ma4_len = input(title="MA4 Len", type=input.integer, defval=89, minval=1, maxval
 // endDate = input(title="End Date", type=input.time, defval=timestamp("31 Dec 2170 23:59"))
 // inDateRange = (time >= startDate) and (time < endDate)
 
+// ----------------------------------------------------------------------------------------------------------
 // STEP 2. Calculate strategy values
+// EMAs Calculations
 deftrend(ma1, ma2, ma3, ma4, rema) => 
-    // tr = True range. Same as tr(false). It is max(high - low, abs(high - close[1]), abs(low - close[1]))
     upper = ma4 + rema * 0.5
     lower = ma4 - rema * 0.5
     uptrend = (ma1 > upper and ma2 > upper and ma3 > upper)
@@ -43,12 +51,12 @@ deftrend(ma1, ma2, ma3, ma4, rema) =>
     // to return
     [uptrend, downtrend]
 
-//EMA Definitions
+//EMA Definitions (Fibonacci numbers)
 ma1 = ema(close, 13)
 ma2 = ema(close, 21)
 ma3 = ema(close, 34)
-ma4 = ema(close, 89)
-rema = ema(tr, 89)
+ma4 = ema(close, 55) //89
+rema = ema(tr, 55) // tr = True range. Same as tr(false). It is max(high - low, abs(high - close[1]), abs(low - close[1]))
 //Define Up and Down Trend for Trend Arrows at Top and Bottom of Screen
 [uptrend,downtrend] = deftrend(ma1, ma2, ma3, ma4, rema)
 
@@ -72,18 +80,50 @@ counter = 0
 
 emaFast = ma1ac1
 emaSlow = ma2ac1
+
+// SQUEEZE Calculations
+// histogram
+val = linreg(close - avg(avg(highest(high, lengthKC), lowest(low, lengthKC)),sma(close,lengthKC)), lengthKC,0)
+shadedRed = (val < 0) and (val > nz(val[1])) //choose shaded red when long
+shadedGreen = (val > 0) and (val < nz(val[1])) //choose shaded green when short
+// Calculate BB
+source = close
+basis = sma(source, length)
+dev = multKC * stdev(source, length)
+upperBB = basis + dev
+lowerBB = basis - dev
+
+// Calculate KC
+ma = sma(source, lengthKC)
+range = tr
+rangema = sma(range, lengthKC)
+upperKC = ma + rangema * multKC
+lowerKC = ma - rangema * multKC
+
+// squeeze or not?
+sqzOn  = (lowerBB > lowerKC) and (upperBB < upperKC) //no order when squeeze on
+sqzOff = (lowerBB < lowerKC) and (upperBB > upperKC) //we will trade only when sqzOff
+noSqz  = (sqzOn == false) and (sqzOff == false)
+
+
 // With that five day gap we account for days when the market is closed
 //               the bar's time    1 day       1w   10weeks
-backtestWindow = time > (timenow - 86400000 *  7    * 10)
-//Conservative Entry True/False Condition -> xác định điểm vào lệnh theo tín hiệu close crossover với ema.
+backtestWindow = time > (timenow - 86400000 *  7    * 100)
+
+// ----------------------------------------------------------------------------------------------------------
+// XÁC ĐỊNH ENTRY cho UPTREND và DOWNTREND
+// Entry by EMAs -> xác định điểm vào lệnh theo tín hiệu close crossover với ema.
 // Long khi emafase > emaslow + close trước và close hiện tại cắt đường emafast. Short khi ngược lại.
 // tương đối giống với việc phá bollinger bands. Bổ sung thêm điều kiện UpTrend
 // Tìm phương án bổ sung thêm điều kiện lastkiss để hình thành lastkisstrade
-entryUpTrend = upTrend and (emaFast > emaSlow) and (close[1] < emaFast) and (close > emaFast) ? 1 : 0
-entryDnTrend = downTrend and (emaFast < emaSlow) and (close[1] > emaFast) and (close < emaFast) ? 1 : 0
-//Definition for Conseervative Entry Up and Down PlotArrows
-// codiff = entryUpTrend == 1 ? entryUpTrend : 0
-// codiff2 = entryDnTrend == 1 ? entryDnTrend : 0
+// entryUpTrend = upTrend and (emaFast > emaSlow) and (close[1] < emaFast) and (close > emaFast) ? 1 : 0
+// entryDnTrend = downTrend and (emaFast < emaSlow) and (close[1] > emaFast) and (close < emaFast) ? 1 : 0
+
+// Entry by squeeze histogram
+entryUpTrend = upTrend and sqzOff and shadedRed ? 1 : 0
+entryDnTrend = downTrend and sqzOff and shadedGreen ? 1 : 0
+
+// ----------------------------------------------------------------------------------------------------------
 // STEP 3. Determine long trading conditions
 // implement the time stop that has us close trades after 8 bars. Always exit without checking backtestwindow
 // exitLong = (barssince(enterLong) > 8)
@@ -97,7 +137,7 @@ exitLong = (strategy.position_size > 0)
 enterShort = entryDnTrend and backtestWindow and (strategy.position_size == 0)
 // exitShort = (strategy.position_size < 0) and (crossover(fast, slow) or (barssince(enterShort) > 3))
 exitShort = (strategy.position_size < 0) 
-
+// ----------------------------------------------------------------------------------------------------------
 // STEP 5. Output strategy data
 // plot(series=fast, color=color.blue, title="fast")
 // plot(series=slow, color=color.navy, title="slow")
@@ -122,12 +162,14 @@ fill(p3, p4, color=color.silver, transp=88)
 // bgColour = enterLong ? green : enterShort ? red : na
 // bgcolor(color=bgColour, transp=90)
 
+// ----------------------------------------------------------------------------------------------------------
 // RISK MANAGEMENT
+rr_base = atr(63)
 // swing stop loss
-sw_sl = atr(63)
+sw_sl = rr_base * 1.5
 // highestHigh = abs(highest(high, 7) - strategy.position_avg_price) // highestHigh in last 7 candles
 // lowestLow = abs(lowest(low, 7) - strategy.position_avg_price) //last 7 candles
-sw_tp = sw_sl * 2
+sw_tp = rr_base * 2
 long_sl = strategy.position_avg_price - sw_sl
 long_tp = strategy.position_avg_price + sw_tp
 short_sl = strategy.position_avg_price + sw_sl
@@ -136,6 +178,7 @@ short_tp = strategy.position_avg_price - sw_tp
 // long_tp = strategy.position_avg_price * 1.02
 // short_sl = strategy.position_avg_price * 1.01
 // short_tp = strategy.position_avg_price * 0.98
+// ----------------------------------------------------------------------------------------------------------
 
 // STEP 6. Submit entry orders
 strategy.entry(id="eL", long=true, when=enterLong) //, stop=sw_sl, limit=sw_tp
@@ -156,4 +199,6 @@ fill(s1, s2, color=color.silver, transp=68)
 // strategy.order(id="xS", long=true, when=exitShort)
 strategy.exit(id="xL", from_entry="eL", limit=long_tp, stop=long_sl, when=exitLong)
 strategy.exit(id="xS", from_entry="eS", limit=short_tp, stop=short_sl, when=exitShort)
+
+// ----------------------------------------------------------------------------------------------------------
 
