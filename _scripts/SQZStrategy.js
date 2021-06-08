241049e771466e674563cd01de71bbbc12885422
Leo Tran <loitranduc@gmail.com>
2021-06-08 22:18:58 +0700

    updated-by-loitd

--- _scripts/SQZStrategy.js
+++ _scripts/SQZStrategy.js
@@ -2,29 +2,30 @@
 // Mục tiêu: lợi nhuận 3% vốn/tháng -> 1 tuần 1 lệnh -> 10% vốn 1 lệnh
 // Profit Factor: số tiền kiếm được trên mỗi đơn vị tiền thua mất = gross profit/gross loss
 
-// Hiện theo backtest, chiến thuật giao dịch này không phù hợp với các khung thời gian nhỏ. 
-// Đối với cặp BTCUSD, 1D, 20/2/20/1.5/hold6/1.5/3/100/false////true/4 có PF 6.63 percentage 67% với 6 giao dịch
-// Đối với cặp AUDUSD, 4h, 20/2/20/1.5/hold6/1.5/3/100/false////true/4 có PF 5.9, percentage 80% với khoảng 20 giao dịch/100 ngày
-// Đối với cặp GBPUSD, 4h, 20/2/20/1.5/hold6/1.5/3/100/false////true/4 có PF 3.4, percentage 67% với khoảng 18 giao dịch/100 ngày
-
 // © loi9985
 //@version=4
 // Nếu để calc_on_every_tick sẽ khó khăn trong việc apply realtime
-strategy(title="SQZStrategy", overlay=true, initial_capital=10000, default_qty_type=strategy.cash, default_qty_value=2700, pyramiding=0, slippage=2, calc_on_every_tick=false)
+strategy(   title="SQZStrategy", overlay=true, initial_capital=100, default_qty_type=strategy.fixed, default_qty_value=1, pyramiding=0, slippage=2, calc_on_every_tick=false, commission_value=0.01, commission_type=strategy.commission.percent)
 // All Declarations
 lengthBB 		= input(21, title="BB Length") // độ dài đường BB. 20 for a little early than 21
 mult 			= input(2.0,title="BB MultFactor") // tham số mult cho BB
 lengthKC 		= input(21, title="KC Length") // độ dài đường KC
 multKC 			= input(1.5, title="KC MultFactor")
-sqzMin  		= input(4, title="SQZ Min") //optimized
+sqzMin  		= input(1, title="SQZ Min") //optimized
 sqzMax	    	= input(39, title="SQZ Max")
 // -----------------------------------------------------------------------
-stoploss 		= input(100, title="Stoploss in PIP") // tỷ lệ loss chấp nhận
-takeprofit 		= input(2000, title="TakeProfit in PIP") // tỷ lệ profit chấp nhận
+stoploss 		= input(1000, title="Stoploss in PIP") // tỷ lệ loss chấp nhận
+takeprofit 		= input(3000, title="TakeProfit in PIP") // tỷ lệ profit chấp nhận
 // -----------------------------------------------------------------------
-minCBodyRatio   = input(0.5, title="Min Candle Body Ratio")
-// Trending EMA
-emaPeriod       = input(200, title="Trending EMA Period")
+minCBodyRatio   = input(0.6, title="Min Candle Body Ratio")
+// Trending EMAs ---------------------------------------------------------
+withTrending    = input(false, title="Apply trending filter?")
+ema1            = input(34, title="Trending EMA 1 Period")
+ema2            = input(89, title="Trending EMA 2 Period")
+ema3            = input(200, title="Trending EMA 3 Period")
+// -----------------------------------------------------------------------
+fromDate         = input(30, title="From date")
+toDate           = input(0, title="To date")
 // SQUEEZE Calculations ---------------------------------------------------------------------------------
 // histogram
 val = linreg(close - avg(avg(highest(high, lengthKC), lowest(low, lengthKC)),sma(close,lengthKC)), lengthKC,0)
@@ -32,6 +33,8 @@ shadedRed 	= (val < 0) and (val > nz(val[1])) //choose shaded red when long
 shadedGreen = (val > 0) and (val < nz(val[1])) //choose shaded green when short
 lightRed 	= (val < 0) and (val <= nz(val[1])) //nz = Replaces NaN values with zeros (or given value) in a series
 lightGreen 	= (val > 0) and (val >= nz(val[1]))
+firstShadedRed = lightRed[1] and shadedRed
+firstShadedGreen = lightGreen[1] and shadedGreen
 // Calculate BB
 basis 		= sma(close, lengthBB)
 dev 		= multKC * stdev(close, lengthBB)
@@ -42,20 +45,27 @@ ma = sma(close, lengthKC)
 rangema 	= sma(tr, lengthKC)
 upperKC 	= ma + rangema * multKC
 lowerKC 	= ma - rangema * multKC
-// squeeze or not? Nguyên tắc chung là không trade tại thời điểm Squeeze do tt không có định hướng rõ ràng và có thể dẫn tới nhiều biến động bất thường.
+
+// ============================================================================================================
+// QUALIFICATIONS
+// ============================================================================================================
+// Back Test Window
+// With that five day gap we account for days when the market is closed. Chỉ tính cho đến ngày hôm qua
+//               the bar's time    1 day       1w   10weeks				to        1d        last
+backtestWindow = time > (timenow - 86400000 * fromDate) and time < (timenow - 86400000 * toDate) ? 1 : 0
+// backtestWindow = 1
+
+// Squeeze qualifications
 sqzOn  = (lowerBB > lowerKC) and (upperBB < upperKC) //no order when squeeze on
 sqzOff = (lowerBB < lowerKC) and (upperBB > upperKC) //we will trade only when sqzOff 
 noSqz  = (sqzOn == false) and (sqzOff == false)
 //Histogram cross zero?
-// isZERO = min( ema(val,20)/100, 0.00099 ) // giá trị có thể coi là 0, có tính đến các giá trị trung bình
-isZERO = sqzMin // giá trị có thể coi là 0, có tính đến các giá trị trung bình
-// isHisCrossZero = abs(val) <= isZERO or (val[1] > 0 and val < 0) or (val[1] < 0 and val > 0) ? 1 : 0
 isSQZQualified = abs(val) >= abs(sqzMin) and abs(val) < abs(sqzMax) ? 1 : 0
-// Back Test Window
-// With that five day gap we account for days when the market is closed. Chỉ tính cho đến ngày hôm qua
-//               the bar's time    1 day       1w   10weeks				to        1m        last
-backtestWindow = time > (timenow - 86400000 * 10) and time < (timenow - 60000 * 1) ? 1 : 0
-// backtestWindow = 1
+
+// Trending qualification
+// ema34 > ema89 > ema200 + ema34 > ema34[1]
+isUpTrend   = ema(close, ema1) > ema(close, ema2) and ema(close, ema2) > ema(close, ema3) and ema(close, ema2) > ema(close, ema2)[1] 
+isDownTrend = ema(close, ema1) < ema(close, ema2) and ema(close, ema2) < ema(close, ema3) and ema(close, ema2) < ema(close, ema2)[1]
 
 // RISK MANAGEMENT --------------------------------------------------------------------------------------
 long_sl 	= strategy.position_avg_price - (stoploss * syminfo.mintick)
@@ -64,15 +74,17 @@ short_sl 	= strategy.position_avg_price + (stoploss * syminfo.mintick)
 short_tp 	= strategy.position_avg_price - (takeprofit * syminfo.mintick)
 long_break 	= high >= long_tp or low <= long_sl ? 1 : 0 // the CLOSE price touch stoploss or takeprofit
 short_break = low <= short_tp or high >= short_sl ? 1 : 0 // trong thực tế chỉ cần HIGH hoặc LOW chạm là đã bị dừng -> phải để HIGH & LOW mới đúng
+
 // Candles calculations ---------------------------------------------------------------------------------
 candleHeight = abs(high-low)
 candleBody = abs(close-open)
 isCandleQualified = candleBody/candleHeight >= minCBodyRatio ? 1 : 0
+
 // ----------------------------------------------------------------------------------------------------------
 // ENTRY by EMAs cross/break
 // Lọc candle color: có 1 thanh đó xuất hiện trong uptrend, nhưng không được có 2 thanh đỏ liên tiếp xuất hiện trong uptrend do có thể là dấu hiệu downtrend. TT với downtrend.
-entryUP = shadedRed and isCandleQualified and isSQZQualified and (high < ema(close, emaPeriod)) 
-entryDN = shadedGreen and isCandleQualified and isSQZQualified and (low > ema(close, emaPeriod))
+entryUP = firstShadedRed and isCandleQualified and isSQZQualified and (not withTrending or isUpTrend) // (high < ema(close, emaPeriod)) 
+entryDN = firstShadedGreen and isCandleQualified and isSQZQualified and (not withTrending or isDownTrend) // (low > ema(close, emaPeriod))
 // ----------------------------------------------------------------------------------------------------------
 // STEP 3. Determine long trading conditions and manually limits
 enterLong 	= entryUP and backtestWindow and (strategy.position_size == 0) ? 1 : 0
